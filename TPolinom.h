8ebbc3363b2e5cec9d448006f81d9d96bb419cec
Катышев Павел Александрович <katyshev.p@ccam.cmc.unn.net>
2019-02-25 13:37:37 +0300

    +=

--- TPolinom.h
+++ TPolinom.h
@@ -3,7 +3,11 @@
 #include "TMonom.h"
 #pragma once
 
-class TPolinom :public THeadList <TMonom>
+class TPolinom :public THeadList <TMonom>	
 {
+public:
 	void InsMonom(const TMonom &n);
+	void operator *=(double d);
+	void operator *=(TMonom &m);
+	void operator +=(const TPolinom q);
 };
