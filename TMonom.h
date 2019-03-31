caf163b9b6c9533bc5496fa4f5e240b11fdea9b2
katyshevp <katyshevpavel2000@gmail.com>
2019-03-31 18:23:44 +0300

    almost done

--- TMonom.h
+++ TMonom.h
@@ -1,5 +1,6 @@
-#include <iostream>
 #pragma once
+#include <iostream>
+using namespace std;
 
 struct TMonom 
 {
@@ -7,75 +8,105 @@ struct TMonom
 	int PowX, PowY, PowZ;
 };
 
-bool operator < (const TMonom& m1, const TMonom& m2) 
-{
-	if (m1.PowX < m2.PowX)
-		return true;
-	if (m1.PowX == m2.PowX) 
-	{
-		if (m1.PowY < m2.PowY)
-			return true;
-		if (m1.PowY == m2.PowY) 
-		{
-			if (m1.PowZ < m2.PowZ)
-				return true;
-			else
-				return false;
-		}
-		else
-			return false;
-	}
-	else
-		return false;
-}
-
-bool operator > (const TMonom& m1, const TMonom& m2) 
-{
-	if (m1.PowX > m2.PowX)
-		return true;
-	if (m1.PowX == m2.PowX)
-	{
-		if (m1.PowY > m2.PowY)
-			return true;
-		if (m1.PowY == m2.PowY)
-		{
-			if (m1.PowZ > m2.PowZ)
-				return true;
-			else
-				return false;
-		}
-		else
-			return false;
-	}
-	else
-		return false;
-}
-
 bool operator == (const TMonom& m1, const TMonom& m2) 
 {
-	if (m1.PowX == m2.PowX)
-	{
-		if (m1.PowY == m2.PowY)
-		{
-			if (m1.PowZ == m2.PowZ)
-				return true;
-			else
-				return false;
-		}
-		else
-			return false;
-	}
+	if (m1.PowX == m2.PowX&&m1.PowY == m2.PowY&&m1.PowZ == m2.PowZ)
+		return true;
+	else
+		return false;
+}
+
+bool operator >= (const TMonom& m1, const TMonom& m2) 
+{
+	if (m1 == m2)return true;
+	else if (m1.PowX > m2.PowX)return true;
+	else if (m1.PowY > m2.PowY)return true;
+	else if (m1.PowZ > m2.PowZ)return true;
+	else
+		return false;
+}
+
+bool operator <=(const TMonom& m1, const TMonom& m2) 
+{
+	if (m1 == m2)return true;
+	else if (m1.PowX < m2.PowX)return true;
+	else if (m1.PowY < m2.PowY)return true;
+	else if (m1.PowZ < m2.PowZ)return true;
 	else
 		return false;
 }
 
 bool operator != (const TMonom& m1, const TMonom& m2) 
 {
-	if (m1.PowX != m2.PowX)
-		return true;
-	if (m1.PowY != m2.PowY)
-		return true;
-	if (m1.PowZ != m2.PowZ)
-		return true;
-	return false;
+	return !(m1 == m2);
+}
+
+bool operator > (const TMonom& m1, const TMonom& m2) 
+{
+	if (m1.PowX > m2.PowX)return true;
+	else if (m1.PowY > m2.PowY)return true;
+	else if (m1.PowZ > m2.PowZ)return true;
+	else
+		return false;
+}
+
+bool operator < (const TMonom& m1, const TMonom& m2) 
+{
+	if (m1.PowX < m2.PowX)return true;
+	else if (m1.PowY < m2.PowY)return true;
+	else if (m1.PowZ < m2.PowZ)return true;
+	else
+		return false;
+}
+
+istream &operator>>(istream &istr, TMonom &m)
+{
+	int coeff, x, y, z;
+	std::cout << "-----------------------------------------------------" << endl;
+	std::cout << "Coeff:";
+	istr >> coeff;
+	std::cout << "Power of X:";
+	istr >> x;
+	std::cout << "Power of Y:";
+	istr >> y;
+	std::cout << "Power of Z:";
+	istr >> z;
+	m.PowX = x;
+	m.PowY = y;
+	m.PowZ = z;
+	m.coeff = coeff;
+	return istr;
+}
+
+ostream& operator<<(ostream &ostr, const TMonom &m)
+{
+	if (m.coeff != 0)
+	{
+		if (m.PowX == 0 && m.PowY == 0 && m.PowZ == 0)
+			ostr << m.coeff;
+		else
+		{
+			if (m.coeff != 1)
+				ostr << m.coeff << " ";
+			if (m.PowX != 0)
+			{
+				ostr << "x";
+				if (m.PowX != 1)
+					ostr << "^" << m.PowX << " ";
+			}
+			if (m.PowY != 0)
+			{
+				ostr << "y";
+				if (m.PowY != 1)
+					ostr << "^" << m.PowY << " ";
+			}
+			if (m.PowZ != 0)
+			{
+				ostr << "z";
+				if (m.PowZ != 1)
+					ostr << "^" << m.PowZ << " ";
+			}
+		}
+	}
+	return  ostr;
 }
--- TPolinom.h
+++ TPolinom.h
@@ -1,13 +1,187 @@
-#include <iostream>
-#include "TheadList.h"
-#include "TMonom.h"
 #pragma once
+#include "THeadList.h"
+#include "TMonom.h"
+#include <iostream>
 
-class TPolinom :public THeadList <TMonom>	
+class TPolinom :public THeadList<TMonom> 
 {
 public:
 	void InsMonom(const TMonom &n);
 	void operator *=(double d);
-	void operator *=(TMonom &m);
-	void operator +=(const TPolinom q);
+	void operator *=(TMonom &n);
+	void operator +=(TMonom &n) 
+	{ 
+		InsMonom(n); 
+	}
+	void operator +=(TPolinom &n);
+
+	void operator *=(const int &n);
+	void operator -=(TPolinom &n);
+	void operator +=(double d);
+	void operator -=(double d);
+	TPolinom();
+	friend ostream& operator<<(ostream &ostr, const TPolinom &n);
+	friend istream& operator>>(istream &istr, TPolinom &n);
 };
+
+TPolinom::TPolinom()
+{
+	TMonom m;
+	m.coeff = 0;
+	m.PowX = m.PowY = 0;
+	m.PowZ = -1;
+	pHead->val = m;
+}
+
+void TPolinom::InsMonom(const TMonom &n) 
+{
+	if (size == 0)
+		InsFirst(n);
+	else
+		for (Reset(); !IsEnd(); GoNext())
+		{
+			if (pCurr->val == n)
+			{
+				pCurr->val.coeff += n.coeff;
+				if (pCurr->val.coeff == 0)
+				{
+					DelCurr();
+					break;
+				}
+			}
+			if (n > pCurr->val)
+			{
+				InsCurr(n);
+				break;
+			}
+		}
+}
+
+void TPolinom::operator*=(double d) 
+{
+	for (Reset(); !IsEnd(); GoNext())
+		pCurr->val.coeff *= d;
+}
+
+void TPolinom::operator *= (TMonom &n) 
+{
+	for (Reset(); !IsEnd(); GoNext())
+	{
+		pCurr->val.coeff *= n.coeff;
+		if (pCurr->val.coeff == 0)
+		{
+			DelCurr();
+			break;
+		}
+		pCurr->val.PowX += n.PowX;
+		pCurr->val.PowY += n.PowY;
+		pCurr->val.PowZ += n.PowZ;
+	}
+}
+
+void TPolinom::operator+=(TPolinom &n) 
+{
+	Reset();
+	n.Reset();
+	while (1)
+	{
+		if (pCurr->val > n.pCurr->val)GoNext();
+		else if (pCurr->val < n.pCurr->val) 
+		{
+			InsCurr(n.pCurr->val);
+			n.GoNext();
+		}
+		else
+		{
+			if (pCurr->val.PowZ == -1 || n.pCurr->val.PowZ == -1)
+				break;
+			if ((n.pCurr->val.coeff + pCurr->val.coeff) == 0)
+			{
+				DelCurr();
+				n.GoNext();
+			}
+			else {
+				pCurr->val.coeff = n.pCurr->val.coeff + pCurr->val.coeff;
+				n.GoNext();
+				GoNext();
+			}
+		}
+	}
+}
+
+ostream& operator<<(ostream  &ostr, TPolinom &p)
+{
+	if (p.size == 0)
+		cout << 0;
+	else
+	{
+		for (p.Reset(); !p.IsEnd(); p.GoNext())
+		{
+			if (p.pos != 0)
+				if (p.pCurr->val.coeff > 0)
+					ostr << "+" << p.pCurr->val;
+				else
+					ostr << p.pCurr->val;
+			else
+				ostr << p.pCurr->val;
+		}
+	}
+	return ostr;
+}
+
+istream& operator>>(istream &istr, TPolinom &n)
+{
+	int k;
+	int i = 0;
+	std::cout << "How many peaces: ";
+	istr >> k;
+	while (i<k)
+	{
+		std::cout << "Monom number:" << i + 1 << endl;
+		TMonom m1;
+		istr >> m1;
+		n.InsMonom(m1);
+		i++;
+		std::cout << "-----------------------------------------------------" << endl;
+	}
+	return istr;
+}
+
+void TPolinom::operator*=(const int &n)
+{
+	for (Reset(); !IsEnd(); GoNext())
+	{
+		pCurr->val.coeff *= n;
+	}
+}
+
+void TPolinom::operator-=(TPolinom &n)
+{
+	n *= -1;
+	this->operator+=(n);
+}
+
+void TPolinom::operator+=(double d)
+{
+	if (pLast->val.PowX == 0)
+	{
+		if (pLast->val.PowY == 0)
+		{
+			if (pLast->val.PowZ == 0)
+				pLast->val.coeff += d;
+		}
+	}
+	else
+	{
+		TMonom m1;
+		m1.coeff = d;
+		m1.PowX = m1.PowY = m1.PowZ = 0;
+		InsLast(m1);
+	}
+}
+
+void TPolinom::operator-=(double d)
+{
+	d *= -1;
+	this->operator+=(d);
+}
