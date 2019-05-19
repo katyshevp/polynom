3cc59c989b81238fd7117c26c0d649aa0a828d02
katyshevp <katyshevpavel2000@gmail.com>
2019-05-19 14:37:01 +0300

    a

--- THeadList.h
+++ THeadList.h
@@ -1,194 +1,46 @@
 #pragma once
-#include "TLink.h"
-#include <iostream>
-
-
 template <class T>
-class THeadList 
+struct TLink
+{
+	T val;
+	TLink *pNext;
+};
+template <class T>
+class THeadList
 {
 public:
-	int pos, size;
-	TLink<T> *pHead, *pFirst;
+	THeadList();		//Конструктор по умолчанию
+	~THeadList();		//Деструктор
+						//protected :	//Для работы тестов требуется публичный доступ
 public:
-	void InsFirst(const T elem);
-	void InsCurr(const T elem);
-	void DelCurr();
-	void InsElem(const T elem);
+	TLink <T> *pHead,	//Указатель на голову
+		*pFirst,		//Указатель на первый элемент в списке
+		*pLast,			//Указатель на последний элемент в списке
+		*pPrev,			//Указатель на предыдущее звено
+		*pCurr;			//Указатель на текущее звено
+	int pos;			//Позиция звена в списке
+	int size;			//Размер списка
+	void InsCurr(const T & elem);	//Добавить звено между предыдущем и текущем звеном
+	void InsFirst(const T &elem);	//Вставить звено в начало  списка
+	void InsLast(const T &elem);	//Вставить звено в конец списка
+	void DelCurr();	//Удалить текущее звено
+	void Reset();		//Обнулить счетчики
+	void GoNext();		//Нужные указатели передвигает вправо на 1 звено
+	bool IsEnd();		// Проверка на конец списка
 	void PrintList();
-	void InsLast(const T elem);
-	THeadList();
-	~THeadList();
-	TLink<T>  *pPrev, *pCurr, *pLast;
-	void Reset();
-	void GoNext();
-	bool IsEnd();
-	T GetCurr();
 };
 
 template <class T>
-THeadList<T>::THeadList() 
+THeadList <T>::THeadList()
 {
-	pHead = new TLink<T>;
+	pHead = new TLink <T>;
 	pHead->pNext = pHead;
-	pFirst = pCurr = pPrev = pLast = pHead;
+	pFirst = pCurr = pLast = pPrev = pHead;
 	size = 0;
-	pos = -1;
+	pos = 0;
 }
-
 template <class T>
-void THeadList<T>::InsFirst(const T elem) 
-{
-	TLink<T> *tmp = new TLink<T>;
-	tmp->val = elem;
-	if (size > 0)
-	{
-		pHead->pNext = tmp;
-		tmp->pNext = pFirst;
-		if (size == 1)pLast = tmp->pNext;
-		pFirst = tmp;
-		size++;
-		pos++;
-	}
-	else
-	{
-		pHead->pNext = tmp;
-		tmp->pNext = pHead;
-		pFirst = tmp;
-		pLast = tmp;
-		pLast->pNext = pHead;
-		size++;
-		pos = 0;
-		pCurr = pFirst;
-	}
-}
-
-template <class T>
-void THeadList<T>::InsCurr(const T elem) 
-{
-	TLink <T> *tmp = new TLink <T>;
-	tmp->val = elem;
-	if (pos == 0 && pPrev == pHead)
-	{
-		pFirst = tmp;
-	}
-	pPrev->pNext = tmp;
-	tmp->pNext = pCurr;
-	if (pCurr == pHead)
-	{
-		pLast = tmp;
-	}
-	pCurr = tmp;
-	if (size == 0)
-	{
-		pos = 0;
-		pLast = tmp;
-		pLast->pNext = pHead;
-		pFirst = tmp;
-	}
-	size++;
-}
-
-template <class T>
-void THeadList<T>::DelCurr() 
-{
-	if (pCurr != pHead && size > 0)
-	{
-		TLink<T> *tmp = pCurr;
-		pPrev->pNext = tmp->pNext;
-		delete tmp;
-		pCurr = pPrev->pNext;
-		size--;
-		if (size == 0)
-		{
-			pFirst = pLast = pPrev = pCurr = pHead;
-			pos = -1;
-		}
-	}
-}
-
-template <class T>
-void THeadList<T>::InsElem(const T elem) 
-{
-	TLink<T> *tmp = new TLink<T>;
-	tmp->val = elem;
-	if (pHead->pNext == pHead)
-	{
-		pFirst = tmp;
-		pLast = tmp;
-		pHead->pNext = pFirst;
-		pFirst->pNext = pHead;
-		pLast = pFirst;
-		pCurr = pFirst;
-		pos = 0;
-		size++;
-	}
-	else if (pFirst == pLast)
-	{
-		pLast = tmp;
-		pFirst->pNext = pLast;
-		pLast->pNext = pHead;
-		size++;
-	}
-	else
-	{
-		pLast->pNext = tmp;
-		pLast = tmp;
-		tmp->pNext = pHead;
-	}
-}
-
-template <class T>
-void THeadList<T>::PrintList() 
-{
-	TLink<T> *tmp = pFirst;
-	while (tmp != pHead)
-	{
-		std::cout << tmp->val << std::endl;
-		tmp = tmp->pNext;
-	}
-	std::cout << std::endl;
-}
-
-template <class T>
-void THeadList<T>::Reset()
-{
-	if (size == 0)
-		throw - 1;
-	pCurr = pFirst;
-	pPrev = pHead;
-	pos = 1;
-}
-
-template <class T>
-void THeadList<T>::GoNext() 
-{
-	if (pCurr != pHead)
-	{
-		pPrev = pCurr;
-		pCurr = pCurr->pNext;
-		pos++;
-	}
-	else
-		throw (-1);
-}
-
-template <class T>
-bool THeadList<T>::IsEnd() 
-{
-	return(pCurr == pHead);
-}
-
-template <class T>
-T THeadList<T>::GetCurr() 
-{
-	if (pCurr != pHead)
-		return pCurr->val;
-	else
-		throw (-1);
-}
-
-template <class T>
-THeadList<T>::~THeadList() 
+THeadList <T >::~THeadList()
 {
 	pCurr = pFirst;
 	while (pCurr != pHead)
@@ -201,11 +53,139 @@ THeadList<T>::~THeadList()
 }
 
 template <class T>
-void THeadList<T>::InsLast(const T elem)
+void THeadList <T> ::InsCurr(const T &elem)
+{
+	if (pCurr == pHead && pPrev == pLast)
+	{
+		InsLast(elem);
+		pCurr = pLast;
+		pos = size;
+	}
+	else
+	{
+		TLink <T> *tmp = new TLink <T>;
+		tmp->val = elem;
+		pPrev->pNext = tmp;
+		tmp->pNext = pCurr;
+		if (size == 0)
+		{
+			pos = 1;
+			pLast = tmp;
+			pFirst = tmp;
+		}
+		if (pos == 1)
+		{
+			pFirst = tmp;
+		}
+		pCurr = tmp;
+		size++;
+	}
+}
+template <class T>
+void THeadList <T> ::InsFirst(const T &elem)
+{
+	TLink<T> *tmp = new TLink<T>;
+	tmp->val = elem;
+	if (size > 0)
+	{
+		pHead->pNext = tmp;
+		tmp->pNext = pFirst;
+		if (size == 1)
+			pLast = tmp->pNext;
+		pFirst = tmp;
+		size++;
+		pos++;
+		if (pos == 2)
+		{
+			pPrev = tmp;
+		}
+	}
+	else
+	{
+		pHead->pNext = tmp;
+		tmp->pNext = pHead;
+		pFirst = tmp;
+		pLast = tmp;
+		pLast->pNext = pHead;
+		size++;
+		pos = 1;
+		pCurr = pFirst;
+	}
+}
+template <class T>
+void THeadList <T> ::InsLast(const T & elem)
 {
 	TLink<T> *tmp = new TLink<T>;
 	tmp->val = elem;
 	pLast->pNext = tmp;
 	tmp->pNext = pHead;
+	if (size == 0)
+	{
+		pFirst = tmp;
+		pCurr = tmp;
+		pos = 1;
+	}
 	pLast = tmp;
+	size++;
+
+}
+template <class T>
+void THeadList <T>::DelCurr()
+{
+	if (size == 0)
+		throw "Error";
+	TLink<T> *tmp = pCurr;
+	pPrev->pNext = tmp->pNext;
+	delete tmp;
+	pCurr = pPrev->pNext;
+	size--;
+	if (pos > size)
+	{
+		pos = 0;
+		pLast = pPrev;
+	}
+	if (pos == 1)
+		pFirst = pCurr;
+	if (size == 0)
+	{
+		pFirst = pLast = pPrev = pCurr = pHead;
+		pos = 0;
+	}
+}
+template <class T>
+void THeadList <T> ::Reset()
+{
+	if (size == 0)
+		throw "Error";
+	pCurr = pFirst;
+	pPrev = pHead;
+	pos = 1;
+}
+template <class T>
+void THeadList <T>::GoNext()
+{
+	if (IsEnd())
+		throw "Error";
+	pPrev = pCurr;
+	pCurr = pCurr->pNext;
+	if (pCurr != pHead)
+		pos++;
+	else
+		pos = 0;
+}
+template <class T>
+bool THeadList<T>::IsEnd()
+{
+	return (pCurr == pHead);
+}
+
+template <class T>
+void THeadList<T>::PrintList() {
+	TLink<T> *tmp = pFirst;
+	while (tmp != pHead)
+	{
+		std::cout << tmp->val << std::endl;
+		tmp = tmp->pNext;
+	}
+	std::cout << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,79 @@
 #include "THeadList.h"
-#include "TMonom.h"
+#include <cstdlib>
 #include "TPolinom.h"
-#include <iostream>
 
-using namespace std;
-
-void main()
+int main()
 {
-	
+	double m;
+	int r , k;
+	TPolinom p, q,copy;
+	M1:
+	cout << "The first polinom :" << endl;
+	cin >> p;
+	system("cls");
+	cout << "The second polinom :" << endl;
+	cin >> q;
+	cout << "The multiplication number :" << endl;
+	cin >> m;
+	system("cls");
+	cout << "The first polinom: " << p << endl;
+	cout << "The second polinom: " << q << endl;
+	cout << "Choose the operation: " << endl;
+	cout << "1)+" << endl;
+	cout << "2)-" << endl;
+	cout << "3)*" << endl;
+	cin >> r;
+	if (r == 1)
+	{
+		cout << "OPERATION +" << endl;
+		p += q;
+		cout << "p+q=" << p << endl;
+		cout << "Do you want to countinue?" << endl;
+		cout << "1)Yes" << endl;
+		cout << "2)No" << endl;
+		cin >> k;
+		if (k = 1)
+			goto M1;
+		if (k = 2)
+		{
+			system("cls");
+			cout << "----EXIT--------EXIT--------EXIT--------EXIT--------EXIT--------EXIT----" << endl;
+		}
+	}
+	if (r == 2)
+	{
+		cout << "OPERATION -" << endl;
+		p -= q;
+		cout << "p-q=" << p << endl;
+		cout << "Do you want to countinue?" << endl;
+		cout << "1)Yes" << endl;
+		cout << "2)No" << endl;
+		cin >> k;
+		if (k = 1)
+			goto M1;
+		if (k = 2)
+		{
+			system("cls");
+			cout << "----EXIT--------EXIT--------EXIT--------EXIT--------EXIT--------EXIT----" << endl;
+		}
+	}
+	if (r == 3)
+	{
+		cout << "OPERANION *" << endl;
+		p *= m;
+		cout << "p*m=" << p << endl;
+		cout << "Do you want to countinue?" << endl;
+		cout << "1)Yes" << endl;
+		cout << "2)No" << endl;
+		cin >> k;
+		if (k = 1)
+			goto M1;
+		if (k = 2)
+		{
+			system("cls");
+			cout << "----EXIT--------EXIT--------EXIT--------EXIT--------EXIT--------EXIT----" << endl;
+		}
+	}
+	system("pause");
+
 }
